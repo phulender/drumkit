7f5284f165e848981463e5ff5bdcd2246305bb6a
phulender <64048153+phulender@users.noreply.github.com>
2020-08-07 12:40:52 +0530

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//detecting button press
+
+var numberofDrumbutton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberofDrumbutton; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+
+        var buttoninnerHTML = this.innerHTML;
+
+        makeSound(buttoninnerHTML);
+    });
+
+}
+
+//detecting key press
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("/sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("/sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("/sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("/sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("/sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("/sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("/sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        default:
+            console.log(buttoninnerHTML)
+            break;
+    }
+
+}
